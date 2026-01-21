b6a172b3f7f8ea9ab7fcb5641cea3aeb4df97e14
Valentina Massa <106406416+massavalentina@users.noreply.github.com>
2026-01-21 13:01:32 -0300

    test deploy first attempt

--- TesisGestorApi/obj/Debug/net8.0/TesisGestorApi.AssemblyInfo.cs
+++ TesisGestorApi/obj/Debug/net8.0/TesisGestorApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesisGestorApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0f05c032d350c038347d3ab21ce630674f4cdaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0653f8470fece7f4c9e549b862a8d42dfa5ccd81")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesisGestorApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesisGestorApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
