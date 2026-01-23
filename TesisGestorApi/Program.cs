5d30c89ca565b4e9f0670e6405a40814152da082
Valentina Massa <106406416+massavalentina@users.noreply.github.com>
2026-01-23 17:09:46 -0300

    fix migrations autogenerated in development env

--- TesisGestorApi/Program.cs
+++ TesisGestorApi/Program.cs
@@ -32,10 +32,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
+if (!app.Environment.IsDevelopment())
 {
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        db.Database.Migrate();
+    }
 }
 
 // Swagger 
