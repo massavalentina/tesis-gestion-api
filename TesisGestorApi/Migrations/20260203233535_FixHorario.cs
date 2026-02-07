38093e3407d46f259acd8de6f490dac062787f99
joaquincarcano <joaquin.carcano@mi.unc.edu.ar>
2026-02-07 15:21:07 -0300

    Fix Horario

---
+++ TesisGestorApi/Migrations/20260203233535_FixHorario.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesisGestorApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixHorario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "HorarioSalida",
+                table: "Horarios",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "HorarioEntrada",
+                table: "Horarios",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "HorarioSalida",
+                table: "Horarios",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "HorarioEntrada",
+                table: "Horarios",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+        }
+    }
+}
