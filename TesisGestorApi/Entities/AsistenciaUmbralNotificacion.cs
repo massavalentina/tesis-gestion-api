637b8a82a3509a0f1233ce63baf11008d6319fdf
francoh98 <139725098+francoh98@users.noreply.github.com>
2026-05-02 22:58:55 -0300

    Fix bug AST-03-HU-001

--- TesisGestorApi/Entities/AsistenciaUmbralNotificacion.cs
+++ TesisGestorApi/Entities/AsistenciaUmbralNotificacion.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System.ComponentModel.DataAnnotations;
 
 namespace TesisGestorApi.Entities
@@ -14,15 +14,6 @@ namespace TesisGestorApi.Entities
         public int AnioLectivo { get; set; }
         public int Umbral { get; set; } // 10/15/20/25
 
-        public int CantidadEnviados { get; set; } = 0; // 0..3
-        public DateTime ProximoEnvioUtc { get; set; }
-
-        [MaxLength(20)]
-        public string Estado { get; set; } = "PENDIENTE"; // PENDIENTE/COMPLETADO
-
         public DateTime CreadoUtc { get; set; }
-        public DateTime? UltimoEnvioUtc { get; set; }
-        public string? UltimoError { get; set; }
-
     }
 }
